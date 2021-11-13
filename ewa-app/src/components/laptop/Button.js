00883394bcb98d1fd2460a0df2c283e4ca2a2ea3
Ozioma <oziomaokoroafor@gmail.com>
2021-11-13 19:16:59 +0100

    Ewa app commit

---
+++ ewa-app/src/components/laptop/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = (props) =>{
+    return(
+        <div className={"button"}>
+            o
+        </div>
+    )
+}
+
+export default Button
--- ewa-app/src/components/laptop/Keyboard.js
+++ ewa-app/src/components/laptop/Keyboard.js
@@ -1,13 +1,19 @@
 import React from "react";
+import Button from "./Button";
 const Keyboard = (props)=>{
     return (
         <div className={"keyboard"}>
             <div>
-                {props.name} Keyboard
-                made in
+                <Button/>
             </div>
-            <div style={{color: "gold", textAlign : "center", fontWeight: "bold" }}>
-                 {props.madeIn}
+            <div>
+                <div>
+                {/*    {props.name} Keyboard*/}
+                {/*    made in*/}
+                {/*</div>*/}
+                {/*<div style={{color: "gold", textAlign : "center", fontWeight: "bold" }}>*/}
+                {/*     {props.madeIn}*/}
+                </div>
             </div>
         </div>
     )
