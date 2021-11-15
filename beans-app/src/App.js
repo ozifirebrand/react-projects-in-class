3e304a21392e40fa31368e671807b7d9dd8ffb13
Ozioma <oziomaokoroafor@gmail.com>
2021-11-15 08:56:07 +0100

    Good stuff commit

--- beans-app/src/App.js
+++ beans-app/src/App.js
@@ -2,13 +2,23 @@ import React from "react";
 import PhoneNumber from "./components/forms/PhoneNumber";
 import FirstName from "./components/forms/FirstName";
 import LastName from "./components/forms/LastName";
+import Password from "./components/forms/Password";
+import Register from "./components/buttons/Register";
 function App(){
   return(
-      <div>
+      <div className={"container"}>
+          <Register className ={"topButton"}/>
+          <div className={"welcomeText"}>
+              <h3>Welcome to Ewa
+              </h3>
+              Where your cravings for<br/>
+              Beans are satisfied
+          </div>
           <PhoneNumber/>
           <FirstName/>
           <LastName/>
-
+          <Password/>
+          <Register className ={"lowerButton"}/>
       </div>
   )
 }
