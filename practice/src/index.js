71ed2637f9e764ef8bb4a56ea2b27e8b19e9f591
Ozioma <oziomaokoroafor@gmail.com>
2021-11-16 21:47:03 +0100

    Practice first commit

---
+++ practice/src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom";
+
+// const name = "Ozi oma"
+// const element = <h1>Ibo la chi, {name}</h1>
+ReactDOM.render(
+    <h1>Hello world</h1>,
+    document.getElementById("root")
+);
