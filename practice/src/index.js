533bfd5cb5537e167a9a4fec7c706dc5e354cd0a
Ozioma <oziomaokoroafor@gmail.com>
2021-11-17 08:02:37 +0100

    Practice commit 2

--- practice/src/index.js
+++ practice/src/index.js
@@ -10,9 +10,31 @@ const user = {
     lastName: "Orige"
 }
 
+function getGreeting(user){
+    if (user){
+        return <h1>Hello, {formatName(user)}! </h1>
+    }
+    return <h1>Hi Stranger.</h1>
+}
+
+function tick(){
+    const element = (
+        <div>
+            <h1>
+                Hello, world!
+            </h1>
+            <h2>
+                It is {new Date().toLocaleTimeString()}.
+            </h2>
+        </div>
+    );
+    ReactDOM.render(element, document.getElementById("root"))
+}
+
+setInterval(tick, 1000);
 
 const name = "Ozi Oma"
-const element = <h1>Ibo la chi, {name}, {formatName(user)}</h1>
+const element = <h1>Ibo la chi, {name}, {getGreeting(user)}</h1>
 ReactDOM.render(
     element,
     document.getElementById("root")
