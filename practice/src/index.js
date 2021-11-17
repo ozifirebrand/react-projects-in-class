6409de2881b1335b40541eae089a7a9f2c59e952
Ozioma <oziomaokoroafor@gmail.com>
2021-11-17 18:39:47 +0100

    Index commit

--- practice/src/index.js
+++ practice/src/index.js
@@ -31,11 +31,11 @@ function tick(){
     ReactDOM.render(element, document.getElementById("root"))
 }
 
-setInterval(tick, 1000);
-
 const name = "Ozi Oma"
 const element = <h1>Ibo la chi, {name}, {getGreeting(user)}</h1>
 ReactDOM.render(
     element,
     document.getElementById("root")
 );
+
+setInterval(tick, 1000);
