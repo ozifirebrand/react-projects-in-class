5a900e0cbc6292d31c7561b6d341469c3a7cd29a
Ozioma <oziomaokoroafor@gmail.com>
2021-11-19 18:38:17 +0100

    Practice commit- changed component clock to a class

--- practice/src/index.js
+++ practice/src/index.js
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom";
+import React from "react";
 
 function formatName(user){
     return user.firstName + " " + user.lastName;
@@ -16,24 +17,25 @@ function getGreeting(user){
     return <h1>Hi Stranger.</h1>
 }
 
-function Clock(props){
-    return(
-        <div>
-            <h1>Hello fam</h1>
-            <h2>It is {props.time.toLocaleTimeString()}.</h2>
-        </div>
-    )
+class Clock extends React.Component{
+    render() {
+        return (
+            <div>
+                <h1>Hello fam!</h1>
+                <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
 }
 
 function tick(){
-    ReactDOM.render(<Clock time= {new Date()}/>, document.getElementById("root"))
+    ReactDOM.render(<Clock date = {new Date()}/>, document.getElementById("root"))
 }
 
 const name = "Ozi Oma"
 const element = <h1>Ibo la chi, {name}, {getGreeting(user)}</h1>
 ReactDOM.render(
-    element,
-    document.getElementById("root")
+    element, document.getElementById("root")
 );
 
 setInterval(tick, 1000);
