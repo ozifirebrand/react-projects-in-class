dabd891b782e5c521981e40f8535ee41fcc452ae
Ozioma <oziomaokoroafor@gmail.com>
2021-11-19 19:18:20 +0100

    Added new components to my project

--- practice2/src/index.js
+++ practice2/src/index.js
@@ -1,5 +1,45 @@
 import ReactDOM from "react-dom";
 
+import React from "react";
+
+function formatName(user){
+    return user.firstName + " " + user.lastName;
+}
+
+const user = {
+    firstName:"Ada Nnaya",
+    lastName: "Orige"
+}
+
+function getGreeting(user){
+    if (user){
+        return <h1>Hello, {formatName(user)}! </h1>
+    }
+    return <h1>Hi Stranger.</h1>
+}
+
+class Clock extends React.Component{
+    render() {
+        return (
+            <div>
+                <h1>Hello fam!</h1>
+                <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+}
+
+function tick(){
+    ReactDOM.render(<Clock date = {new Date()}/>, document.getElementById("root"))
+}
+
+const name = "Ozi Oma"
+const element1 = <h1>Ibo la chi, {name}, {getGreeting(user)}</h1>
+ReactDOM.render(
+    element1, document.getElementById("root")
+);
+
+setInterval(tick, 1000);
 
 function formatDate(date) {
     return date.toLocaleDateString();
