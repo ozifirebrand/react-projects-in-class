62f958245048e59a7e3595a95d4cbbf07a6e4c7b
Ozioma <oziomaokoroafor@gmail.com>
2021-11-19 11:43:04 +0100

    Beans app another commit

--- beans-app/src/ewa/components/auth/register/Register.js
+++ beans-app/src/ewa/components/auth/register/Register.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import Input from "../../reusables/Input";
-import "./login.css"
-import img from "../../../../assets/background.svg"
+import "./register.css"
+import img from "../../../../assets/background.svg";
 import imgb from "../../../../assets/background2.svg"
 import Button from "../../reusables/button";
-
-const Login = () => {
-
+import Input from "../../reusables/Input";
+const Register = () => {
     const inputFields = [
+        {label: "FirstName", placeholder: "Enter your firstname"},
+        {label: "LastName", placeholder: "Enter your lastname"},
         {label: "Email", placeholder: "Enter your email"},
         {label: "Password", placeholder: "Enter your password"},
     ]
     return (
-        <div className={"login-container"}>
-            <div className={"login-header"}>
+        <div className={"register-container"}>
+            <div className={"register-header"}>
                 <div className={"ewa-logo"}>
+
                 </div>
                 <button>
-                    REGISTER
+                    LOGIN
                 </button>
             </div>
             <img className={"top-circle"} src = {img} alt={"circlebackground"}/>
@@ -29,12 +30,13 @@ const Login = () => {
                 </p>
             </div>
             <div className={"form-container"}>
-                {inputFields.map((field, index)=> <Input key = {index} field={ field}/>)}
+                {inputFields.map((field, index)=>
+                    <Input key = {index} field={ field}/>)}
             </div>
-            <Button name = "Login"/>
+            <Button name = "Register"/>
             <img className={"bottom-circle"} src={imgb} alt={"circlebackground"}/>
         </div>
     );
 };
 
-export default Login;
+export default Register;
--- beans-app/src/ewa/views/auth/Auth.js
+++ beans-app/src/ewa/views/auth/Auth.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Login from "../../components/auth/login/Login";
+import Register from "../../components/auth/register/Register";
 
 const Auth = () => {
     return (
         <div>
-            <Login/>
+            {/*{<Login/>}*/}
+            <Register/>
         </div>
     );
 };
